44320e70a90cf187ca182369d3fd5a159bdf5d1e
amlakgirma <amlakgirma@yahoo.com>
2017-06-05 15:55:08 -0400

    i got 10 bomb randomly put in my table but all turn red one time

--- app.js
+++ app.js
@@ -1,23 +1,87 @@
 var count = 5;
-var container;
+var minesAll = 10;
+var permit = false;
+
 $(document).ready(function() {
-  container = $('.feild');
-  var row = $("<div calss='line'></div");
-  var item = $("<div class ='item'></div>");
-  var currentrow;
-  var currentitem;
-  for(var x=0; x < count; x ++) {
-    currentrow = row.clone();
-    container.append(currentrow);
-    for(var y =0; y< count; y ++) {
-      currentitem = item.clone();
-      currentrow.append(currentitem);
+	var container = $(".field");
+	var row = $("<div class='line'></div>");
+	var item = $("<div class='item'></div>");
+	var currentRow;
+	var currentItem;
 
-    }
-  }
-$(".item").on("click", function() {
+	for (var i = 0; i < count; i++) {
+		currentRow = row.clone();
+		container.append(currentRow);
+		for (var j = 0; j < count; j++) {
+			currentItem = item.clone();
+		 currentRow.append(currentItem);
+		}
+	}
 
-    $(this).css('background-color', 'yellow');
-
+	$(".new-game button").on("click", function() {
+		currentGame = new Game();
+		currentGame.MinesCreate();
+    currentGame.showMines();
+		permit = true;
 	});
+
 });
+
+function Game() {
+	this.cells = [];
+  console.log(this);
+	this.MinesCreate = function() {
+		var indexindexJ, cond;
+		var indArr = [];
+		var elem = [];
+		for (var i = 0; i < count; i++) {
+			this.cells[i] = [];
+
+		}
+		for (var i = 0; i < minesAll; i++) {
+			do {
+				indexI = Math.floor(Math.random()*count);
+				indexJ = Math.floor(Math.random()*count);
+				elem = [indexI, indexJ];
+				cond = false;
+				for (var j = 0; j < indArr.length; j++) {
+
+					if (indArr[j][0] == elem[0]
+
+						&& indArr[j][1] == elem[1]) {
+						cond = true;
+						break;
+					}
+				}
+			} while (cond);
+			indArr.push(elem);
+			this.cells[indexI][indexJ] = -1;
+      console.log(elem);
+
+		}
+	},
+
+
+
+	this.showMines = function() {
+		$(".item").html("").removeClass("green").addClass("open");
+
+		for (var i = 0; i < count; i++) {
+			for (var j = 0; j < count; j++) {
+				if (this.cells[i][j] == -1)
+					$(".line").eq(i).find(".item").eq(j)
+					.html("&#x2602;").addClass("bold");
+
+
+			}
+
+		}
+    $(".bold").css("background-color", 'red');
+
+	},
+
+  this.clearGame = function() {
+		$(".item").html("").removeClass("open")
+		.removeClass("red").removeClass("bold").removeClass("yellow");
+	}
+}
