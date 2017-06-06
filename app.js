f0fe02f1b4f1b019ef864c272bee6d1014d47ba1
amlakgirma <amlakgirma@yahoo.com>
2017-06-06 06:20:40 -0400

    add restriction cells but when i ckicked play game all show up

--- app.js
+++ app.js
@@ -23,37 +23,7 @@ $(document).ready(function() {
     showMines();
 		permit = true;
 	});
-
 });
-
-	$(".item").on("mousedown", function(event) {
-		var parI, parJ, parentLine, items;
-		if (permit) {
-			parentLine = $(this).parents(".line");
-			parI = $(".line").index(parentLine);
-
-			items = parentLine.find(".item");
-			parJ = items.index(this);
-			if (event.which == 1) {
-				if (currentGame.cells[parI][parJ] == -2)
-					currentGame.explosionEnd();
-				else {
-					$(this).addClass("open");
-					$(this).text(currentGame.cells[parI][parJ]);
-					currentGame.checkEnd();
-				}
-			} else if (event.which == 3) {
-				if ($(this).hasClass("red")) {
-					$(this).removeClass("red").html("");
-					ourPlayer.removeMine();
-				} else {
-					$(this).html("&#9967;").addClass("red");
-				    ourPlayer.addMine();
-					currentGame.checkEnd();
-				}
-			}
-		}
-	});
 function Game() {
 	this.cells = [];
 	this.MinesCreate = function() {
@@ -82,18 +52,18 @@ function Game() {
 			} while (BoolCond);
 			indArr.push(elem);
 			this.cells[indexI][indexJ] = -2;
-
+			console.log(elem);
 		}
 
 	},
 	this.showMines = function() {
-		$(".item").html("").removeClass("green").addClass("open");
+
 		for (var i = 0; i < count; i++) {
 			for (var j = 0; j < count; j++) {
 				if (this.cells[i][j] == -2)
 					$(".line").eq(i).find(".item").eq(j)
 					.html("&#x2602;").addClass("bold");
-
+						$(".bold").css("background-color", 'yellow');
 			}
 		}
 
