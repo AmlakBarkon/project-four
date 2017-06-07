42e349d604128fb56f90a329a4cf89f51d901538
amlakgirma <amlakgirma@yahoo.com>
2017-06-07 13:03:40 -0400

    object added to items

--- app.js
+++ app.js
@@ -1,34 +1,92 @@
 var count = 4;
-var minesAll = 4;
+var minesAll = 8;
 var permit = false;
+var beginTime, endTime, timer;
 
 $(document).ready(function() {
 	var container = $(".field");
-	var row = $("<div class='line'></div>");//create row has class line
-	var item = $("<div class='item'></div>");// create cell for the row
-	var RowCurrent;
+	var row = $("<div class='line'></div>");
+	var item = $("<div class='item'></div>");
+	var currentRow;
 	var currentItem;
 
 	for (var i = 0; i < count; i++) {
-		RowCurrent = row.clone();// clone row and append to container
-		container.append(RowCurrent);
+		currentRow = row.clone();
+		container.append(currentRow);
 		for (var j = 0; j < count; j++) {
 			currentItem = item.clone();
-		 RowCurrent.append(currentItem);
+		 currentRow.append(currentItem);
 		}
 	}
+
 	$(".new-game button").on("click", function() {
-		Game();
-		MinesCreate();
-    showMines();
+    currentGame = new Game();
+		currentGame.createMines();
 		permit = true;
-	});
 
 });
+
+$(".item").on("mousedown", function(event) {
+	var parI, parJ, parentLine, items;
+	if (permit) {
+		parentLine = $(this).parents(".line");
+		parI = $(".line").index(parentLine);
+
+		items = parentLine.find(".item");
+		parJ = items.index(this);
+		if (event.which == 1) {
+			if (currentGame.this.cells == -1)
+				currentGame.explosionEnd();
+			else {
+				$(this).addClass("open");
+				$(this).text(currentGame.cells[parI][parJ]);
+
+			}
+		} else if (event.which == 3) {
+			if ($(this).hasClass("red")) {
+				$(this).removeClass("red").html("");
+				ourPlayer.removeMine();
+			} else {
+				$(this).html("&#9967;").addClass("red");
+					ourPlayer.addMine();
+				currentGame.checkEnd();
+			}
+		}
+	}
+});
+});
+
 function Game() {
-	this.cells = [];
-	this.MinesCreate = function() {
-		var indexI, indexJ, BoolCond;
+	this.cells = [
+		[
+			{ bomb: false, selected: false, value: null },
+			{ bomb: false, selected: false, value: null },
+			{ bomb: false, selected: false, value: null },
+			{ bomb: false, selected: false, value: null },
+		],
+		[
+			{ bomb: false, selected: false, value: null, id:null},
+			{ bomb: false, selected: false, value: null, id:null},
+			{ bomb: false, selected: false, value: null, id:null },
+			{ bomb: false, selected: false, value: null, id:null },
+		],
+		[
+			{ bomb: false, selected: false, value: null, id:null },
+			{ bomb: false, selected: false, value: null, id:null },
+			{ bomb: false, selected: false, value: null, id:null },
+			{ bomb: false, selected: false, value: null, id:null },
+		],
+		[
+			{ bomb: false, selected: false, value: null, id:null },
+			{ bomb: false, selected: false, value: null, id:null },
+			{ bomb: false, selected: false, value: null, id:null },
+			{ bomb: false, selected: false, value: null, id:null },
+		],
+
+	];
+
+	this.createMines = function() {
+		var indI, indJ, cond;
 		var indArr = [];
 		var elem = [];
 		for (var i = 0; i < count; i++) {
@@ -37,39 +95,39 @@ function Game() {
 		}
 		for (var i = 0; i < minesAll; i++) {
 			do {
-				indexI = Math.floor(Math.random()*count);
-				indexJ = Math.floor(Math.random()*count);
-				elem = [indexI, indexJ];
-				BoolCond = false;
-				console.log(indArr);
+				indI = Math.floor(Math.random()*count);
+				indJ = Math.floor(Math.random()*count);
+				elem = [indI, indJ];
+				cond = false;
 				for (var j = 0; j < indArr.length; j++) {
-
 					if (indArr[j][0] == elem[0]
-
 						&& indArr[j][1] == elem[1]) {
-						BoolCond = true;
+						cond = true;
 						break;
 					}
 				}
-			} while (BoolCond);
+			} while (cond);
 			indArr.push(elem);
-			this.cells[indexI][indexJ] = -2;
-
+			this.cells[indI][indJ] = { bomb: true, selected: true, value: 1, id:2};
+			console.log(indArr);
 		}
+	},
 
-	}
+	this.explosionEnd = function() {
+		this.showMines();
+		permit = false;
+	},
+
 	this.showMines = function() {
-
+		$(".item").html("").removeClass("green").addClass("open");
 		for (var i = 0; i < count; i++) {
 			for (var j = 0; j < count; j++) {
-				if (this.cells[i][j] == -2)
+				if (this.cells== -1)
 					$(".line").eq(i).find(".item").eq(j)
 					.html("&#x2602;").addClass("bold");
-
+					console.log("thank you");
 			}
-
 		}
 
 	}
-
-};
+ }
