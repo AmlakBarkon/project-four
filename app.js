25cf1daa67f4697204863a070bd6369c3b406e50
amlakgirma <amlakgirma@yahoo.com>
2017-06-08 10:44:24 -0400

    display mines but couldn't figure out how to count the number of mines around the cell

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-var count = 4;
-var minesAll = 8;
+var count = 5;
+var minesAll = 10;
 var permit = false;
-var beginTime, endTime, timer;
-
+//=================================Create a table
 $(document).ready(function() {
 	var container = $(".field");
 	var row = $("<div class='line'></div>");
 	var item = $("<div class='item'></div>");
 	var currentRow;
 	var currentItem;
-
+//===============================append items
 	for (var i = 0; i < count; i++) {
 		currentRow = row.clone();
 		container.append(currentRow);
@@ -18,80 +17,49 @@ $(document).ready(function() {
 		 currentRow.append(currentItem);
 		}
 	}
-
+//==============================================
 	$(".new-game button").on("click", function() {
-    currentGame = new Game();
+		currentGame = new Game();
 		currentGame.createMines();
 		permit = true;
 
 });
+//==============
 
-$(".item").on("mousedown", function(event) {
-	var parI, parJ, parentLine, items;
-	if (permit) {
-		parentLine = $(this).parents(".line");
-		parI = $(".line").index(parentLine);
+	$(".item").on("mousedown", function(event) {
+		var parI, parJ, parentLine, items;
+		if (permit) {
+			parentLine = $(this).parents(".line");//
 
-		items = parentLine.find(".item");
-		parJ = items.index(this);
-		if (event.which == 1) {
-			if (currentGame.this.cells == -1)
-				currentGame.explosionEnd();
-			else {
-				$(this).addClass("open");
-				$(this).text(currentGame.cells[parI][parJ]);
+			parI = $(".line").index(parentLine);//get rows index
 
-			}
-		} else if (event.which == 3) {
-			if ($(this).hasClass("red")) {
-				$(this).removeClass("red").html("");
-				ourPlayer.removeMine();
-			} else {
-				$(this).html("&#9967;").addClass("red");
-					ourPlayer.addMine();
-				currentGame.checkEnd();
+			items = parentLine.find(".item");
+
+			parJ = items.index(this);//get column index
+
+			if (event.which == 1) {
+				if (currentGame.cells[parI][parJ] == -3)//where i put mines
+					currentGame.explosionEnd();
+				else {
+					$(this).addClass("open");
+
+					$(this).text(currentGame.cells[parI][parJ]);
+
+				}
 			}
 		}
-	}
+	});
 });
-});
-
+//=====================
 function Game() {
-	this.cells = [
-		[
-			{ bomb: false, selected: false, value: null },
-			{ bomb: false, selected: false, value: null },
-			{ bomb: false, selected: false, value: null },
-			{ bomb: false, selected: false, value: null },
-		],
-		[
-			{ bomb: false, selected: false, value: null, id:null},
-			{ bomb: false, selected: false, value: null, id:null},
-			{ bomb: false, selected: false, value: null, id:null },
-			{ bomb: false, selected: false, value: null, id:null },
-		],
-		[
-			{ bomb: false, selected: false, value: null, id:null },
-			{ bomb: false, selected: false, value: null, id:null },
-			{ bomb: false, selected: false, value: null, id:null },
-			{ bomb: false, selected: false, value: null, id:null },
-		],
-		[
-			{ bomb: false, selected: false, value: null, id:null },
-			{ bomb: false, selected: false, value: null, id:null },
-			{ bomb: false, selected: false, value: null, id:null },
-			{ bomb: false, selected: false, value: null, id:null },
-		],
-
-	];
-
+	this.cells = [];
 	this.createMines = function() {
 		var indI, indJ, cond;
 		var indArr = [];
 		var elem = [];
 		for (var i = 0; i < count; i++) {
 			this.cells[i] = [];
-
+			console.log(this.cells[i]);
 		}
 		for (var i = 0; i < minesAll; i++) {
 			do {
@@ -99,35 +67,48 @@ function Game() {
 				indJ = Math.floor(Math.random()*count);
 				elem = [indI, indJ];
 				cond = false;
+
 				for (var j = 0; j < indArr.length; j++) {
-					if (indArr[j][0] == elem[0]
-						&& indArr[j][1] == elem[1]) {
+
+			if (indArr[j][0] == elem[0]&& indArr[j][1] == elem[1]) {
 						cond = true;
 						break;
 					}
 				}
 			} while (cond);
-			indArr.push(elem);
-			this.cells[indI][indJ] = { bomb: true, selected: true, value: 1, id:2};
-			console.log(indArr);
+
+			this.cells[indI][indJ] = -3;
 		}
+
 	},
 
+
+// ================================explosion clicked all display
 	this.explosionEnd = function() {
 		this.showMines();
 		permit = false;
-	},
 
+	},
+//============================================
 	this.showMines = function() {
-		$(".item").html("").removeClass("green").addClass("open");
+		$(".item").html("").removeClass("red").addClass("open");
 		for (var i = 0; i < count; i++) {
 			for (var j = 0; j < count; j++) {
-				if (this.cells== -1)
+				if (this.cells[i][j] == -3)
 					$(".line").eq(i).find(".item").eq(j)
-					.html("&#x2602;").addClass("bold");
-					console.log("thank you");
+					.html("&#128163;");
 			}
 		}
-
+	},
+	//=================show mines when clicked
+	this.showSumsMines = function() {
+		for (var i = 0; i < count; i++) {
+			for (var j = 0; j < count; j++) {
+				if (this.cells[i][j] == -3)
+					$(".line").eq(i).find(".item")
+					.eq(j).removeClass("red");
+			}
+		}
 	}
- }
+
+}
